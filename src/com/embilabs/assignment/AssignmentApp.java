561f0a2d135d6eab1e85161122640299d04720b6
vijayg24 <vijay.garg@cars24.com>
2021-06-20 13:01:38 +0530

    inital commit

---
+++ src/com/embilabs/assignment/AssignmentApp.java
@@ -0,0 +1,15 @@
+package com.embilabs.assignment;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class AssignmentApp {
+
+  public static void main(String[] args) {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+  }
+
+
+}
